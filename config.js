7e81739dad3e374852e564b99b6b352991469dbc
Tiger <47013568+TIGER4EVER@users.noreply.github.com>
2025-04-18 16:36:41 +0200

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  OWNER_IDS: [""], // Bot owner ID's
-  SUPPORT_SERVER: "", // Your bot support server
+  OWNER_IDS: ["468514598517997588"], // Bot owner ID's
+  SUPPORT_SERVER: "https://discord.gg/uNnu6PEf5k", // Your bot support server
   PREFIX_COMMANDS: {
     ENABLED: true, // Enable/Disable prefix commands
     DEFAULT_PREFIX: "!", // Default prefix for the bot
